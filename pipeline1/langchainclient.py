1c85921095d1e6a91c61f6d1c40bcc9109a1fb31
alokd <alok@greymatterz.com>
2024-12-23 14:58:16 -0500

    added gitignore, .env file, langchainclient and pip freeze output file

---
+++ pipeline1/langchainclient.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+from langchain_community.llms import Ollama
+
+load_dotenv()
+
+OLLAMA_SERVER_URL = os.environ['OLLAMA_SERVER_URL']
+print(OLLAMA_SERVER_URL)
+
+print(OLLAMA_SERVER_URL)
+llm = Ollama(model="llama3.2", base_url=OLLAMA_SERVER_URL)
+
+response = llm.invoke("hi, how are you?")
+print(response)
